9806714a3946e2e0040e2fc6b8f0691c901c4b22
tatsuroMF <160145495+tatsuroMF@users.noreply.github.com>
2024-02-24 19:38:45 +0600

    add js2

---
+++ js2/main.js
@@ -0,0 +1,23 @@
+for (let i = 0; i <= 12; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " четное")
+    }
+}
+
+for (let i = 10; i >= 0; i--) {
+    console.log(i)
+}   
+let i = 0
+while (i < 12) {
+    i++
+    if (i % 2 === 0) {
+        console.log(i + " четное")
+    }
+}
+
+
+let j = 0
+while (j < 10) {
+    j++
+    console.log(j)
+}
